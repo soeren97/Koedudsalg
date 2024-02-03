a81234219010fede53da7132db239c132f2d6274
Søren <soeren@langkilde.com>
2024-02-03 16:49:49 +0100

    Added button to get daily repport

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from datetime import datetime, timedelta
 
 from src.filehandling import load_config, save_config
 
@@ -24,12 +25,22 @@ class DateRangeWindow:
         self.end_entry.pack()
 
         # Button to perform an action with the date range
-        self.action_button = tk.Button(
+        self.date_button = tk.Button(
             master,
             text="Submit dates",
             command=self.get_dates,
         )
-        self.action_button.pack()
+
+        self.date_button.pack()
+
+        # Button to perform an action today
+        self.daily_button = tk.Button(
+            master,
+            text="Daily report",
+            command=self.daily_report,
+        )
+
+        self.daily_button.pack()
 
     def get_dates(self) -> None:
         """Get user input from the entry widgets."""
@@ -38,6 +49,28 @@ class DateRangeWindow:
 
         config = load_config("config")
         config["Start_date"], config["End_date"] = start_date, end_date
+        save_config("config", config)
+
+        self.master.destroy()
+
+    def daily_report(self) -> None:
+        """Get user input from the entry widgets."""
+        # Get the current date and time
+        current_datetime = datetime.now()
+
+        # Calculate the date for yesterday
+        yesterday = current_datetime - timedelta(days=1)
+
+        # Get the earliest timestamp for yesterday (midnight)
+        start_date = datetime(yesterday.year, yesterday.month, yesterday.day, 0, 0, 0)
+
+        # # Get the latest timestamp for yesterday (23:59:59)
+        end_date = datetime(yesterday.year, yesterday.month, yesterday.day, 23, 59, 59)
+
+        config = load_config("config")
+        config["Start_date"] = start_date.strftime("%Y-%m-%d %H:%M:%S")
+        config["End_date"] = end_date.strftime("%Y-%m-%d %H:%M:%S")
+
         save_config("config", config)
 
         self.master.destroy()
